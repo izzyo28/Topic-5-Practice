19bc946d0c33f612baf67ba687430deb90583c6d
Stephen Cerruti <scerruti@otabi.com>
2024-07-03 14:29:17 -0700

    Create LocalVariablesAndScope.java

---
+++ LocalVariablesAndScope.java
@@ -0,0 +1,19 @@
+public class LocalVariablesAndScope 
+{
+  public static void main(String[] args)
+  {
+    //Declare and initialize a String variable named 
+    //message that stores the String "I'm from main!".
+    
+    
+    //Use a print statement to display the message variable in the console.
+    
+    
+    //Call printMessage method.
+    
+    
+  }
+  
+  //Create printMessage method here.
+  
+}
