7d6a62c7e81ca8f50c69cac95d96cecd590a210b
izzyo28 <ioelkers@icloud.com>
2024-07-03 22:06:25 +0000

    Izzy Oelkers

--- LocalVariablesAndScope.java
+++ LocalVariablesAndScope.java
@@ -4,16 +4,22 @@ public class LocalVariablesAndScope
   {
     //Declare and initialize a String variable named 
     //message that stores the String "I'm from main!".
-    
-    
+    String message = "I'm from main!";
+
     //Use a print statement to display the message variable in the console.
-    
+    System.out.println(message);
     
     //Call printMessage method.
-    
+    LocalVariablesAndScope.printMessage();
+
+    System.out.println(message);
     
   }
   
   //Create printMessage method here.
+  public static void printMessage () {
+    String message = "I'm from printMessage!";
+    System.out.println(message);
+  }
   
 }
